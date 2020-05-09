1694285044b47047b55c0f83a49347d50dd43b07
irakoze bruce <irabruce20@gmail.com>
2020-05-09 16:17:59 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+/** @format */
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  let btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", () => {
+    question.classList.toggle("show-text");
+
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+  });
+});
